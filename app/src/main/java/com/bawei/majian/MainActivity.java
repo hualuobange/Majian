773cc8082349fe75cdeaf4eec25561aa64f2eeb6
Administrator <1499401007@qq.com>
2017-04-10 18:00:49 +0800

    第二次

--- app/src/main/java/com/bawei/majian/MainActivity.java
+++ app/src/main/java/com/bawei/majian/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+   
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取id
+        initview();
+    }
+
+    private void initview() {
     }
 }
